1828e0c4c9eb3dbb6c7bea518bae3558ee74d671
Jeongah Yi <jeongahyi@gmail.com>
2023-06-23 19:38:49 -0400

    Add product service to call api

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from '../common/product';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      // Map the JSON data from Spring Data REST to Product array
+      map((response) => response._embedded.products)
+    );
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  };
 }
